649bf0d0f120bf072b9ce9f1b8ba3f88b95d5903
zzangjyj0818 <jyj010818@naver.com>
2026-08-16 18:46:59 +0900

    Init Project

---
+++ src/test/java/yeonjae/guard/GuardApplicationTests.java
@@ -0,0 +1,13 @@
+package yeonjae.guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
